453538fe96370212176b922de59f11bbe480fdb2
PhamTrinhDuc <duc78240@gmail.com>
2025-10-03 18:10:03 +0700

    refactor folder: extractors

---
+++ source/embedder/__init__.py
@@ -0,0 +1,6 @@
+from .resnet_extractor import ResNetExtractor
+from .vgg_extractor import VGGExtractor
+from .vit_extractor import ViTExtractor
+from .dino_extractor import DINOv2Extractor
+
+__all__ = ['ResNetExtractor', 'VGGExtractor', 'ViTExtractor', 'DINOv2Extractor']
