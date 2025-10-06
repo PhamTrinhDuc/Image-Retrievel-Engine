7ad13fde832c1fc030c83d8d5a9a20da5bcc8664
PhamTrinhDuc <duc78240@gmail.com>
2025-10-06 18:37:48 +0700

    scripts

--- source/script/insert_embeddings_to_vdb.py
+++ source/script/insert_embeddings_to_vdb.py
@@ -5,29 +5,42 @@ sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 from data_processer.minio_client import MinioClient
 from retriever.loader import ImageEmbeddingLoader
+from configs.helper import DataConfig
 import argparse
 
-BUCKET_NAME = "ecommerce-product-images"
+BUCKET_NAME = DataConfig.bucket_name
+MINIO_ENDPOINT = DataConfig.minio_endpoint
+MINIO_ACCESS_KEY = DataConfig.minio_access_key
+MINIO_SECRET_KEY = DataConfig.minio_secret_key
 
 def get_images_from_minio() -> dict[str, list[str]]: 
-  minio_client = MinioClient(bucket_name=BUCKET_NAME)
+  minio_client = MinioClient(bucket_name=BUCKET_NAME,
+                              minio_endpoint=MINIO_ENDPOINT,
+                              minio_access_key=MINIO_ACCESS_KEY,
+                              minio_secret_key=MINIO_SECRET_KEY)
   categories = minio_client.get_categories()
   image_urls = defaultdict(list)
-  for category in categories: 
+  for category_id, category in enumerate(categories, start=1): 
     images_object = minio_client.get_images_in_category(category=category)
     image_url = [minio_client.get_image_url(object_name=object_name, 
                                             expires_in_seconds=3600*10) for object_name in images_object]
     image_urls[category] = image_url
+    # if category_id == 3: # For testing, remove this line to process all categories
+    #   break 
   
   return image_urls
 
 
-def load_embeddings_to_vdb(model: str, vdb: str, 
+def load_embeddings_to_vdb(model: str, config_model: dict, 
+                            vdb: str, vdb_config: dict, 
                            image_urls: list[str], metadata: list[str], 
                            recreate_collection: bool = False):
     
     loader = ImageEmbeddingLoader(extractor_type=model, 
-                                  vdb_type=vdb)
+                                  extractor_config=config_model,
+                                  vdb_type=vdb, 
+                                  vdb_config=vdb_config)
+    
     if recreate_collection:
       loader.vdb_client.drop_collection()
 
@@ -51,15 +64,17 @@ def main_with_args(args):
     image_urls=image_list, 
     metadata=metadata_list,
     model=args.model,
+    config_model=None,
     vdb=args.vdb,
+    vdb_config={"collection_name": args.collection_name},
     recreate_collection=args.recreate_collection
   )
   
 if __name__ == "__main__":
-  
   parser = argparse.ArgumentParser(description="Insert image embeddings to vector database.")
   parser.add_argument("--model", type=str, default="resnet", help="Model type for embedding extraction.")
   parser.add_argument("--vdb", type=str, default="milvus", help="Vector database type.")
+  parser.add_argument("--collection_name", type=str, default="images", help="Collection name in vector database.")
   parser.add_argument("--recreate_collection", action="store_true", help="Recreate collection in vector database.")
 
   args = parser.parse_args()
--- source/script/insert_images_to_minio.py
+++ source/script/insert_images_to_minio.py
@@ -3,15 +3,25 @@ import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 from data_processer.loader import DataLoader
+from configs.helper import DataConfig
 
-SOURCE_FOLDER = "/mnt/c/Users/Admin/Downloads/archive/animals/animals"
-BUCKET_NAME = "animal-images"
-MAX_WORKERS = 8
-BATCH_SIZE = 50
+SOURCE_FOLDER = DataConfig.data_folder
+BUCKET_NAME = DataConfig.bucket_name
+MAX_WORKERS = DataConfig.num_workers
+BATCH_SIZE = DataConfig.batch_size
+MINIO_ENDPOINT = os.getenv("MINIO_ENDPOINT", "localhost:9000")
+MINIO_ACCESS_KEY = os.getenv("MINIO_ACCESS_KEY", "minioadmin")
+MINIO_SECRET_KEY = os.getenv("MINIO_SECRET_KEY", "minioadmin")  
 
 def main():
-  loader = DataLoader(bucket_name=BUCKET_NAME, source_folder=SOURCE_FOLDER,
-                      max_workers=MAX_WORKERS, batch_size=BATCH_SIZE)
+  loader = DataLoader(bucket_name=BUCKET_NAME, 
+                      source_folder=SOURCE_FOLDER,
+                      minio_endpoint=MINIO_ENDPOINT,
+                      minio_access_key=MINIO_ACCESS_KEY,
+                      minio_secret_key=MINIO_SECRET_KEY,
+                      max_workers=MAX_WORKERS, 
+                      batch_size=BATCH_SIZE, 
+                      size_limit=(150, 1500))  # (min, max) size in pixels
 
   loader.load_images_batch()     # Recommended
   # loader.load_images_parallel() 
