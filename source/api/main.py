78475ea748d0205509744e34d6ac0032c466d49d
PhamTrinhDuc <duc78240@gmail.com>
2025-10-09 14:58:15 +0700

    route api

--- source/api/main.py
+++ source/api/main.py
@@ -12,6 +12,7 @@ import uvicorn
 from api.routes.route_retriever import routes as retriever_router
 from api.routes.route_vdb import routes as vdb_router
 from api.routes.route_embedder import routes as embedder_router
+from api.routes.route_operate import routes as operate_router
 
 app = FastAPI(title="Image Retrieval API", version="1.0")
 
@@ -19,5 +20,6 @@ app = FastAPI(title="Image Retrieval API", version="1.0")
 app.include_router(retriever_router, prefix="/retriever", tags=["retriever"])
 app.include_router(vdb_router, prefix="/vdb", tags=["vdb"])
 app.include_router(embedder_router, prefix="/embedder", tags=["embedder"])
+app.include_router(operate_router, prefix="/operate", tags=["operate"])
 
 # script: uvicorn main:app --host 0.0.0.0 --port 8001 --reload
--- source/api/routes/__init__.py
+++ source/api/routes/__init__.py
@@ -1,9 +1,11 @@
 from .route_embedder import routes as router_embedder
 from .route_retriever import routes as router_retriever
 from .route_vdb import routes as router_vdb
+from .route_operate import routes as router_operate
 
 __all__ = [
     "router_embedder",
     "router_retriever",
     "router_vdb",
+    "router_operate"
 ]
