0932c70fb2e8b652f6b4c63692d565ba91e1bfb0
PhamTrinhDuc <duc78240@gmail.com>
2025-10-09 14:59:11 +0700

    config

--- source/configs/helper.py
+++ source/configs/helper.py
@@ -9,7 +9,7 @@ class DataConfig:
   minio_endpoint: str = os.getenv("MINIO_ENDPOINT", "localhost:9000")
   minio_access_key: str = os.getenv("MINIO_ACCESS_KEY", "minioadmin")
   minio_secret_key: str = os.getenv("MINIO_SECRET_KEY", "minioadmin")
-  batch_size: int = 32
+  batch_size: int = 64
   num_workers: int = 8
   pin_memory: bool = True
   shuffle: bool = True
@@ -23,28 +23,28 @@ DEFAULT_EXTRACTOR_CONFIGS = {
       'model_name': 'resnet34',
       'collection_name': 'resnet_embedding',
       'device': 'cpu',
-      'batch_size': 1,  # Smaller batch for inference
+      'batch_size': 32,  # Smaller batch for inference
       'enable_mixed_precision': False
   },
   'vgg': {
       'model_name': 'vgg16',
       'collection_name': 'vgg_embedding',
       'device': 'cpu',
-      'batch_size': 1,
+      'batch_size': 32,
       'enable_mixed_precision': False
   },
   'vit': {
       'model_name': 'vit_base_patch16_224',
       'collection_name': 'vit_embedding',
       'device': 'cpu',
-      'batch_size': 1,
+      'batch_size': 32,
       'enable_mixed_precision': False
   },
   'dinov2': {
       'model_name': 'dinov2_vitb14',
       'collection_name': 'dinov2_embedding',
       'device': 'cpu',
-      'batch_size': 1,
+      'batch_size': 32,
       'enable_mixed_precision': False
   }
 }
@@ -54,7 +54,7 @@ DEFAULT_SEARCH_CONFIGS = {
   'milvus': {
       'host': 'localhost',
       'port': '19530',
-      'collection_name': 'image_embeddings',
+      'collection_name': 'image_embedding',
       'search_params': {
           'metric_type': 'L2',
           'params': {'nprobe': 16}
